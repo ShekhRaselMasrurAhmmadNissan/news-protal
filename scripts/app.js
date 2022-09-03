cfd0e843988347822a8497528b458c7570e71ba8
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-09-03 16:42:52 +0600

    Modal Data Dynamic load and modal spinner added.

--- scripts/app.js
+++ scripts/app.js
@@ -226,7 +226,7 @@ const displayAllNews = async (data, categoryName) => {
 								></path>
 							</svg>
 						</div>
-						<label for="my-modal-4" class="btn modal-button" onclick="showSingleNews('${_id}')">Read Full News</label>
+						<label for="my-modal-4" class="btn modal-button" onclick="loadSingleNews('${_id}')">Read Full News</label>
 
 					</div>
 				</div>
@@ -236,8 +236,153 @@ const displayAllNews = async (data, categoryName) => {
 	});
 };
 
-const showSingleNews = async (id) => {
-	console.log(id);
+/**
+ * @description This function will take the news ID as parameter and Fetch the news of that ID
+ * @author Shekh Rasel Masrur Ahmmad
+ * @date 03/09/2022
+ * @param {String} id
+ */
+const loadSingleNews = async (id) => {
+	const url = `https://openapi.programming-hero.com/api/news/${id}`;
+	document.getElementById('modal-spinner').classList.remove('hidden');
+	try {
+		const res = await fetch(url);
+		const data = await res.json();
+		displaySingleNews(data.data[0]);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const displaySingleNews = async (news) => {
+	document.getElementById('modal-spinner').classList.add('hidden');
+	const modalBody = document.getElementById('modal-body');
+	const {
+		_id,
+		title,
+		image_url,
+		details,
+		total_view,
+		author: { name: authorName, published_date, img: authorImage },
+		rating: { number: ratingNumber },
+	} = news;
+	modalBody.innerHTML = `
+		<img
+			src="${image_url}"
+			alt=""
+		/>
+
+		<article>
+			<div class="flex items-center mb-4 space-x-4 justify-between w-[95%] mx-auto mt-2">
+				<div class="flex items-center mb-4 space-x-4">
+					<img
+						class="w-16 h-16 rounded-full"
+						src="${authorImage}"
+						alt=""
+					/>
+					<div class="space-y-1 font-medium dark:text-white">
+						<p>
+							${authorName}
+							<time
+								class="block text-sm text-gray-500 dark:text-gray-400"
+								>${published_date}</time
+							>
+						</p>
+					</div>
+				</div>
+				<!-- Views -->
+				<p class="flex">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6 mr-1"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+						/>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+						/>
+					</svg>
+					${total_view}
+				</p>
+				<!-- Ratings -->
+				<div class="flex items-center mb-1">
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>First star</title>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						></path>
+					</svg>
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Second star</title>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						></path>
+					</svg>
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Third star</title>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						></path>
+					</svg>
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Fourth star</title>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						></path>
+					</svg>
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Fifth star</title>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						></path>
+					</svg>
+					<span class="ml-2">${ratingNumber}</span>
+				</div>
+			</div>
+			<p class="mb-2  dark:text-gray-400">
+				${details}
+			</p>
+		</article>
+	`;
 };
 
 /**
